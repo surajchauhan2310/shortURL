9adbf1a762970cdcf3cc4dce13cddaa44c59545b
surajchauhan2310 <23surajrc@gmail.com>
2024-09-03 13:30:34 +0530

    App got updated using the process.env variable and index.js got replaced with the app.ls file to get deployed in the AWS's EC2 instance

--- app.js
+++ app.js
@@ -1,12 +1,13 @@
+require("dotenv").config();
 const express = require("express");
 const mongoose = require("mongoose");
 const router = require("./shortURLRoutes");
 
 const app = express();
 app.use(express.json());
-const PORT = 4600;
+const PORT = process.env.PORT || 8000;
 
-mongoose.connect("mongodb://localhost:27017/Bitly"); //mongoose connection
+mongoose.connect(process.env.MONGO_URL); //mongoose connection
 
 app.use(router);
 //PORT listening
